633a6c17a9f34f667164f0bec27a35185c185b1e
GFlyan <guilhermeflyan.22@gmail.com>
2025-04-19 16:23:40 -0300

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ df
 df = ipeadatapy.metadata()
 df
 
-"""A busca é feita como passagem de parametros especificos das funções ***list_series()*** e ***metadata()***"""
+"""A busca é feita com a passagem de parametros específicos das funções ***list_series()*** e ***metadata()***"""
 
 "\n"
 
